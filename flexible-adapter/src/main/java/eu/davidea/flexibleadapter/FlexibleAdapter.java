5ce62d101f42febfbbb0b25eb2f31e866e028fa4
Александр Горшков <al.gorshkov@gmail.com>
2016-09-21 13:51:25 +0300

    fsdf

--- flexible-adapter/src/main/java/eu/davidea/flexibleadapter/FlexibleAdapter.java
+++ flexible-adapter/src/main/java/eu/davidea/flexibleadapter/FlexibleAdapter.java
@@ -1891,9 +1891,9 @@ public class FlexibleAdapter<T extends IFlexible>
 //            }
 
             //Expand!
-            notifyDataSetChanged();
+//            notifyDataSetChanged();
 //            notifyItemRangeInserted(position - 2, subItemsCount + 2);
-//            notifyItemRangeInserted(position + 1, subItemsCount);
+            notifyItemRangeInserted(position + 1, subItemsCount);
             //Show also the headers of the subItems
             if (!init && headersShown) {
                 int count = 0;
